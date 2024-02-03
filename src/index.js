88a00f97de4e1ca6656d1fa1919fa59058a2fda2
Jacob Ye <gmding777@gmail.com>
2024-02-02 21:25:56 -0800

    attached day data to app

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-import "./style.css";
 import { getWeatherData } from "./weather";
 
 async function buildDivs() {
   const data = await getWeatherData();
   Object.entries(data).forEach((day) => {
-    console.log(day);
-    createDay(day);
+    body.appendChild(createDay(day));
   });
 }
 
 function createDay(day) {
+  const container = document.createElement("div");
   const dayData = day[1].day;
   const maxTempF = document.createElement("div");
   const minTempF = document.createElement("div");
   const dayImage = document.createElement("img");
 
-  
+  maxTempF.innerHTML = dayData.maxtemp_f;
+  minTempF.innerHTML = dayData.mintemp_f;
+  dayImage.src = dayData.condition.icon.substring(2);
+
+  container.appendChild(maxTempF);
+  container.appendChild(minTempF);
+  container.appendChild(dayImage);
+  return container;
+
 }
 
+const body = document.querySelector("body");
+console.log(body);
 buildDivs();
